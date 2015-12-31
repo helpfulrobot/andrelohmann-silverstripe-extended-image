0929becf605648aa87d25661b1e24684807c3d88
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:34:01 +1300

    Converted to PSR-2

--- code/extensions/ExtendedGDBackend.php
+++ code/extensions/ExtendedGDBackend.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 /**
  * extended-image/code/ExtendedGD.php
  * 
@@ -7,18 +7,19 @@
  * 
  */
  
-class ExtendedGDBackend extends DataExtension {
+class ExtendedGDBackend extends DataExtension
+{
     
     /**
      * Merge two Images together
      */
-    public function merge(GDBackend $image){
-        
+    public function merge(GDBackend $image)
+    {
         imagealphablending($this->owner->getImageResource(), false);
-		imagesavealpha($this->owner->getImageResource(), true);
+        imagesavealpha($this->owner->getImageResource(), true);
         
         imagealphablending($image->getImageResource(), false);
-		imagesavealpha($image->getImageResource(), true);
+        imagesavealpha($image->getImageResource(), true);
         
         $srcX = 0;
         $srcY = 0;
@@ -32,99 +33,109 @@ class ExtendedGDBackend extends DataExtension {
         imagecopyresampled($this->owner->getImageResource(), $image->getImageResource(), $dstX, $dstY, $srcX, $srcY, $dstW, $dstH, $srcW, $srcH);
         
         $output = clone $this->owner;
-		$output->setImageResource($this->owner->getImageResource());
-		return $output;
+        $output->setImageResource($this->owner->getImageResource());
+        return $output;
     }
     
     /**
      * blur the image
      */
-    public function blur($intensity) {
+    public function blur($intensity)
+    {
         $image = $this->owner->getImageResource();
         
-        switch($intensity){
+        switch ($intensity) {
             case 'light':
-                for ($x=1; $x<=10; $x++)
+                for ($x=1; $x<=10; $x++) {
                     imagefilter($image, IMG_FILTER_GAUSSIAN_BLUR);
+                }
             break;
             
             case 'strong':
-                for ($x=1; $x<=40; $x++)
+                for ($x=1; $x<=40; $x++) {
                     imagefilter($image, IMG_FILTER_GAUSSIAN_BLUR);
+                }
             break;
             
             case 'normal':
             default:
-                for ($x=1; $x<=25; $x++)
+                for ($x=1; $x<=25; $x++) {
                     imagefilter($image, IMG_FILTER_GAUSSIAN_BLUR);
+                }
             break;
         }
         
         $output = clone $this->owner;
-		$output->setImageResource($image);
-		return $output;
+        $output->setImageResource($image);
+        return $output;
     }
     
-    public function transparentPaddedResize($width, $height) {
-		if(!$this->owner->getImageResource()) return;
-		$width = round($width);
-		$height = round($height);
-		
-		// Check that a resize is actually necessary.
-		if ($width == $this->owner->getWidth() && $height == $this->owner->getHeight()) {
-			return $this->owner;
-		}
-		
-		$newGD = imagecreatetruecolor($width, $height);
-		
-		// Preserves transparency between images
-		imagealphablending($newGD, false);
-		imagesavealpha($newGD, true);
+    public function transparentPaddedResize($width, $height)
+    {
+        if (!$this->owner->getImageResource()) {
+            return;
+        }
+        $width = round($width);
+        $height = round($height);
+        
+        // Check that a resize is actually necessary.
+        if ($width == $this->owner->getWidth() && $height == $this->owner->getHeight()) {
+            return $this->owner;
+        }
+        
+        $newGD = imagecreatetruecolor($width, $height);
+        
+        // Preserves transparency between images
+        imagealphablending($newGD, false);
+        imagesavealpha($newGD, true);
                 
         $transparent = imagecolorallocatealpha($newGD, 0, 0, 0, 127);
                 
-		imagefilledrectangle($newGD, 0, 0, $width, $height, $transparent);
+        imagefilledrectangle($newGD, 0, 0, $width, $height, $transparent);
         imagealphablending($newGD, true);
-		
-		$destAR = $width / $height;
-		if ($this->owner->getWidth() > 0 && $this->owner->getHeight() > 0) {
-			// We can't divide by zero theres something wrong.
-			
-			$srcAR = $this->owner->getWidth() / $this->owner->getHeight();
-		
-			// Destination narrower than the source
-			if($destAR > $srcAR) {
-				$destY = 0;
-				$destHeight = $height;
-				
-				$destWidth = round( $height * $srcAR );
-				$destX = round( ($width - $destWidth) / 2 );
-			
-			// Destination shorter than the source
-			} else {
-				$destX = 0;
-				$destWidth = $width;
-				
-				$destHeight = round( $width / $srcAR );
-				$destY = round( ($height - $destHeight) / 2 );
-			}
-			
-			imagecopyresampled($newGD, $this->owner->getImageResource(), $destX, $destY, 0, 0, $destWidth, $destHeight, $this->owner->getWidth(), $this->owner->getHeight());
-		}
-		$output = clone $this->owner;
-		$output->setImageResource($newGD);
-		return $output;
-	}
+        
+        $destAR = $width / $height;
+        if ($this->owner->getWidth() > 0 && $this->owner->getHeight() > 0) {
+            // We can't divide by zero theres something wrong.
+
+            $srcAR = $this->owner->getWidth() / $this->owner->getHeight();
+        
+            // Destination narrower than the source
+            if ($destAR > $srcAR) {
+                $destY = 0;
+                $destHeight = $height;
+                
+                $destWidth = round($height * $srcAR);
+                $destX = round(($width - $destWidth) / 2);
+            
+            // Destination shorter than the source
+            } else {
+                $destX = 0;
+                $destWidth = $width;
+                
+                $destHeight = round($width / $srcAR);
+                $destY = round(($height - $destHeight) / 2);
+            }
+            
+            imagecopyresampled($newGD, $this->owner->getImageResource(), $destX, $destY, 0, 0, $destWidth, $destHeight, $this->owner->getWidth(), $this->owner->getHeight());
+        }
+        $output = clone $this->owner;
+        $output->setImageResource($newGD);
+        return $output;
+    }
     
-    public function detectedFaceImage($face){
-		if(!$this->owner->getImageResource()) return;
-		
-		if ($face == null){
-			return $this->owner;
-		}
-		
-		$color = imagecolorallocate($this->owner->getImageResource(), 255, 0, 0); //red
-		imagerectangle(
+    public function detectedFaceImage($face)
+    {
+        if (!$this->owner->getImageResource()) {
+            return;
+        }
+        
+        if ($face == null) {
+            return $this->owner;
+        }
+        
+        $color = imagecolorallocate($this->owner->getImageResource(), 255, 0, 0); //red
+        imagerectangle(
             $this->owner->getImageResource(),
             $face['x'],
             $face['y'],
@@ -132,9 +143,9 @@ class ExtendedGDBackend extends DataExtension {
             $face['y']+$face['w'],
             $color
         );
-		
-		$output = clone $this->owner;
-		$output->setImageResource($this->owner->getImageResource());
-		return $output;
-	}
+        
+        $output = clone $this->owner;
+        $output->setImageResource($this->owner->getImageResource());
+        return $output;
+    }
 }
